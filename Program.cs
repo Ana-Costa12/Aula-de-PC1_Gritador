e0440c976d828b04001471673aa858282e6453af
Ana-Costa12 <claudiasantborges@gmail.com>
2021-03-12 21:54:57 -0300

    Torcida!!

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Aula_de_PC1_Gritador
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string mensagem;
+
+            Console.WriteLine("Digite uma mensagem positiva para seus colegas:");
+            mensagem = Console.ReadLine();
+            Console.Clear();
+        
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine($"{mensagem}!!!".ToUpper());
+            Console.ResetColor();
+
+            //Incentivar os que estão a sua volta gera produtividade e construção de caráter pessoal//
+            Console.WriteLine("\n Lembre-se que estamos todos em eterno processo de aprendizagem!\n\n Tecle enter para sair: ");
+            Console.ReadLine();
+            Console.Clear();
+
+
+        }
+    }
+}
